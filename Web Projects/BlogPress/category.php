1c9e0d940b4fc4730c9988e0b20241fde93ae29e
Nimul Islam Mahin <141026311+gitmahin@users.noreply.github.com>
2024-02-05 03:08:13 +0600

    updated

--- Web Projects/BlogPress/category.php
+++ Web Projects/BlogPress/category.php
@@ -1,54 +1,55 @@
 <?php get_header()
     ?>
-<div class="loop-post-d-wrapper">
-    <div class="loop-post-container">
-    <?php
-        if (have_posts()) { ?>
+<main>
+    <div class="loop-post-d-wrapper">
+        <div class="loop-post-container">
             <?php
-            while (have_posts()) {
-                the_post();
-                ?>
-                <div class="ca-post-box">
-                    <div class="ca-post-thumbnail">
-                        <a href="<?php the_permalink() ?>">
-                            <?php the_post_thumbnail() ?>
-                        </a>
+            if (have_posts()) { ?>
+                <?php
+                while (have_posts()) {
+                    the_post();
+                    ?>
+                    <div class="ca-post-box">
+                        <div class="ca-post-thumbnail">
+                            <a href="<?php the_permalink() ?>">
+                                <?php the_post_thumbnail() ?>
+                            </a>
+                        </div>
+                        <div class="ca-post-content">
+                            <ul>
+                                <?php $cate = get_the_category($post->ID);
+                                foreach ($cate as $category) {
+                                    ?>
+                                    <li>
+                                        <?php echo $category->name; ?>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                            <h2>
+                                <?php the_title() ?>
+                            </h2>
+                            <p>
+                                <?php the_excerpt() ?>
+                            </p>
+                            <p class="ca-post-date">
+                                <?php echo get_the_date() ?>
+                            </p>
+                        </div>
+                        <div class="ca-post-button">
+                            <a href="<?php the_permalink() ?>"><button>
+                                    Read more
+                                </button></a>
+                        </div>
                     </div>
-                    <div class="ca-post-content">
-                        <ul>
-                            <?php $cate = get_the_category($post->ID);
-                            foreach ($cate as $category) {
-                                ?>
-                                <li>
-                                    <?php echo $category->name; ?>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                        <h2>
-                            <?php the_title() ?>
-                        </h2>
-                        <p>
-                            <?php the_excerpt() ?>
-                        </p>
-                        <p class="ca-post-date">
-                            <?php echo get_the_date() ?>
-                        </p>
-                    </div>
-                    <div class="ca-post-button">
-                        <a href="<?php the_permalink() ?>"><button>
-                                Read more
-                            </button></a>
-                    </div>
-                </div>
-            <?php } ?>
-        <?php } else { ?>
-            <h1 style="margin-bottom: 500px;">Create Your First Category😃</h1>
-        <?php }
-        ?>
+                <?php } ?>
+            <?php } else { ?>
+                <h1 style="margin-bottom: 500px;">Create Your First Category😃</h1>
+            <?php }
+            ?>
+        </div>
+        <?php catin_pagination() ?>
     </div>
     <?php catin_pagination() ?>
     </div>
-    <?php catin_pagination() ?>
-
-</div>
+</main>
 <?php get_footer() ?>
